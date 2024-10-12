d6ef3ff796b575e43ec103885b8808dd13725d18
Orkhand4 <orkhand4@mail.ru>
2024-10-12 12:07:48 +0400

    props map

--- client/src/components/nav/index.jsx
+++ client/src/components/nav/index.jsx
@@ -2,13 +2,32 @@ import React from 'react'
 import './style.scss'
 
 const Nav = ({logo = 'Ruslan' ,...props}) => {
+  const links = [
+    {
+      title: 'Home',
+      href: '/',
+    },
+    {
+      title: 'About',
+      href: '/about',
+    },
+    {
+      title: 'Contact',
+      href: '/contact',
+    },
+  ]
+
+
+
   return (
     <header>
       <h1>{logo}</h1>
       <nav>
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <a href="#">Contact</a>
+        {
+          links.map((item,index) => {
+            return <a key={index} href={item.href}>{item.title}</a>
+          })
+        }
       </nav>
       <button>{props.btnText}</button>
     </header>
